128ff2555f7770b5183a44a010475dda0847d94f
Crocodin <parmac.codrin@gmail.com>
2026-07-22 10:23:15 +0300

    starting FastAPI

---
+++ backend/app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.concurrency import asynccontextmanager
+
+from .api.detection import route as detection_route
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Perform any startup tasks here
+    print("Starting up the CV Parsing HR Assistant API...")
+    yield
+    # Perform any shutdown tasks here
+    print("Shutting down the CV Parsing HR Assistant API...")
+
+app = FastAPI(
+    title="DRAC prediction API",
+    version="1.0.0",
+    lifespan=lifespan
+)
+
+app.root_path = "/api"
+
+app.include_router(detection_route, prefix="/detection", tags=["Detection"])
